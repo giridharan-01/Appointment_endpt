801eb2e7f0fb04c67772a2666704ceb42433701a
giri <sifugiridharan@gmail.com>
2025-02-04 10:59:08 +0530

    Updating temperature value

--- utils.py
+++ utils.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 
 def init_llm():
-    llm =ChatOpenAI(model="gpt-4o-mini", openai_api_key=os.getenv('OPENAI_API_KEY'))
+    llm =ChatOpenAI(model="gpt-4o-mini", openai_api_key=os.getenv('OPENAI_API_KEY'),temperature = 0)
     return llm
